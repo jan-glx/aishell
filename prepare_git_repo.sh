634c4dd13023dd70856f29aae4389cc4e8e8235b
Jan Gleixner (via AI) <jan.gleixner+AI@gmail.com>
2025-06-24 09:36:04 +0000

    Remove obsolete prepare_git_repo.sh script

--- prepare_git_repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cp -f ~/api.py ./
-cp -fr ~/app ./
-cp -a /etc/systemd/system/fastapi.service ./fastapi.service
-cp -a /etc/nginx/sites-available/shell.gleixner.xyz ./nginx-shell.gleixner.xyz
-cp -a /var/www/shell.gleixner.xyz/openapi.yaml ./openapi.yaml
-
